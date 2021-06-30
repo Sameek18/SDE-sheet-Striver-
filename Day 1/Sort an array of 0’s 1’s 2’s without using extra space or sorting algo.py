b10dbf88454b5ca21239cb2809317029037e9fe2
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-06-30 06:11:40 +0530

    sort 0,1,2

--- Day 1/Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.py
+++ Day 1/Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.py
@@ -1,13 +1,44 @@
+#using counting sort TC:O(n^2)
+
+def Countsort(nums,n):
+    count0,count1,count2=0,0,0
+    for i in range(0,n):
+        if nums[i]==0:
+            count0+=1
+        elif nums[i]==1:
+            count1+=1
+        elif nums[i]==2:
+            count2+=1
+
+    for i in range(0,count1):
+        nums[i]=0
+
+    for i in range(count1,count0+count1):
+        nums[i]=1
+
+    for i in range(count0+count1,n):
+        nums[i]=2
+
+    return(nums)
+
+
+#Using Dutch national flag algo O(n)
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        i,m,j=0,0,len(nums)-1
-        while m<=j:
-            if nums[m]==0:
-                nums[m],nums[i]=nums[i],nums[m]
-                m+=1
-                i+=1
-            elif nums[m]==1:
-                m+=1
-            elif nums[m]==2:
-                nums[m],nums[j]=nums[j],nums[m]
-                j-=1
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<=high):
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                 nums[high],nums[mid]=nums[mid],nums[high]
+                 high-=1
+                 mid+=1
+                    
+        return nums
