adc0ce26db885f6c81d5db5457958812055d9f03
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-07-14 02:39:11 +0530

    nth node from end

--- Day 5/Remove N-th node from back of LinkedList.py
+++ Day 5/Remove N-th node from back of LinkedList.py
@@ -1,17 +1,31 @@
+#two pointer approach
+
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        if head.next==None:
+        #initialize two pointers pointing to head
+        slow=head
+        fast=head
+        count=0
+        #check if list is empty or has only 1 element
+        
+        if head==None or head.next==None:
             return None
-        temp=head
-        while n:
-            temp=temp.next
-            n-=1
-        current=head
-        if temp==None:
-            head=head.next
-            return head
-        while temp.next:
-            current=current.next
-            temp=temp.next
-        current.next=current.next.next
+        else:
+            
+            for i in range(n):      #set a difference of n nodes between slow and fast
+                fast=fast.next  
+        
+            if not fast:              #check if n is greater than size of LL 
+                return head.next
+            
+        #increment both pointers  
+            while fast.next!=None:
+                fast=fast.next
+                slow=slow.next
+
+            slow.next=slow.next.next  #delete node
+
         return head
+
+    
+       
--- Day 5/Reverse a LinkedList.py
+++ Day 5/Reverse a LinkedList.py
@@ -1,3 +1,4 @@
+#Using 2 pointer approach
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
@@ -8,3 +9,24 @@ class Solution:
             prev = curr
             curr = head
         return prev
+    
+    
+    
+#using a stack 
+
+def reverseList(self, head: ListNode) -> ListNode:
+        stack=[]
+        temp=head
+        #put all LL elements in stack
+        while temp:
+            stack.append(temp)
+            temp=temp.next
+        #pop elements prom stack and put back in LL  
+        while len(stack)>0:
+            elem=stack.pop()
+            temp.next=elem
+            temp=elem
+            
+        elem.next=None
+            
+        return head
