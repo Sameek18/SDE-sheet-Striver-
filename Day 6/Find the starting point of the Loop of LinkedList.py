ada56640212fdb687989eebd0ee6a2235b7ffc5c
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-07-19 01:26:45 +0530

    .

--- Day 6/Find the starting point of the Loop of LinkedList.py
+++ Day 6/Find the starting point of the Loop of LinkedList.py
@@ -1,18 +1,33 @@
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        if head==None or head.next==None:
-            return 
+          #initialize 2 nodes pointing to head
         slow=head
         fast=head
-        while fast and fast.next:
-            slow=slow.next
+        isloop=False
+        
+           #checking if LL consists of 1-2 elements
+        if head==None or head.next==None:
+            return None
+        
+           #checking if there is a loop
+        while fast and fast.next: 
             fast=fast.next.next
-            if slow==fast:
-                break
-        if slow!=fast:
-            return 
-        slow=head
-        while slow!=fast:
             slow=slow.next
-            fast=fast.next
-        return slow
+            if slow==fast:      #if there is a loop then slow and fast will meet
+                isloop=True
+                break
+                
+                
+            #find position where loop starts  
+            #the point at which both pointers meet is the start of the loop
+        if isloop:
+            slow=head
+            while slow!=fast:
+                slow=slow.next
+                fast=fast.next
+                
+            return fast
+        
+        
+        return None
+                
