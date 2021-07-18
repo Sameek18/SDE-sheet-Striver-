d682c30aa982337d5108cc45f6fb36f6e4b502ad
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-07-18 07:13:55 +0530

    Linked list updated

--- Day 5/Find middle of LinkedList.py
+++ Day 5/Find middle of LinkedList.py
@@ -1,10 +1,10 @@
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
-        if head.next==None:
+        if head.next==None: #check if LL consis of 1 node
             return head
-        slow=head
+        slow=head        #nitialize 2 pointers
         fast=head
-        while fast and fast.next:
+        while fast and fast.next:   #increment slow by 1 and fast  by 2
             slow=slow.next
             fast=fast.next.next
         return slow
--- Day 6/Detect a cycle in Linked List.py
+++ Day 6/Detect a cycle in Linked List.py
@@ -1,11 +1,13 @@
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        slow=head   #initialize to pointers to head
         fast=head
-        slow=head
         while fast and fast.next:
-            fast=fast.next.next
-            slow=slow.next
-            if fast==slow:
+            fast=fast.next.next     #fast moves by 2
+            slow=slow.next     #slow moves by one
+            
+            if slow==fast:
+                              #if fast and slow meet inside loop return True   
                 return True
         else:
             return False
