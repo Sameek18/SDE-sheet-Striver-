eecf731a77533743d7353661fa1d4dbd003ce87a
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-07-20 08:51:58 +0530

    merge 2 LL

--- Day 5/Merge two sorted Linked List.py
+++ Day 5/Merge two sorted Linked List.py
@@ -1,14 +1,21 @@
-class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1==None and l2==None:
-            return
-        if l1==None:
-            return l2
-        if l2==None and l1:
-            return l1
-        if l1.val<l2.val:
-            l1.next=self.mergeTwoLists(l1.next,l2)
-            return l1
+def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy =ListNode(-1)     #we will take a dummy node
+        current=dummy           #current points to the dummy node 
+        
+        while(l1!=None and l2!=None): #comparing elements in both lists
+            if l1.val<l2.val:       
+                current.next=l1              #smaller node is connected to current node
+                l1=l1.next
+            else:
+                current.next=l2
+                l2=l2.next
+            current=current.next
+            
+            
+            #condition when one of the LL reaches Null 
+        if l1!=None: 
+            current.next=l1
         else:
-            l2.next=self.mergeTwoLists(l1,l2.next)
-            return l2
+            current.next=l2
+            
+        return dummy.next
