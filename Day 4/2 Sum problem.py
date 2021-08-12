9f23002d1bb4930bb70093ea7638ba0b0dda009b
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-08-12 23:34:52 +0530

    2 sum prob

--- Day 4/2 Sum problem.py
+++ Day 4/2 Sum problem.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d={}
+        #initialize empty dictionary
+        store=dict()
+        diff=0
         for i in range(len(nums)):
-            if nums[i] in d:
-                return [d[nums[i]],i]
+            #find pair forming in nums[u]
+            diff=target-nums[i]
+            #store that number in dict if not already there
+            if diff not in store:
+                store[nums[i]]=i
+            #if that element is found return position of both elements
             else:
-                d[target-nums[i]]=i
+                return [store[diff],i]
