2e68207268f557a295474f64c73779b7e0883f59
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-08-22 02:05:50 +0530

    Update N-th root of an integer (use binary search) (square root, cube root, ..).py

---
+++ Day11/N-th root of an integer (use binary search) (square root, cube root, ..).py
@@ -0,0 +1,20 @@
+def findnroot(x,n):
+    if x>=0 and x<=1:  #check if x lies between 0,1
+        l,h=0,1
+    else:
+        l,h=1,x
+        
+    eps=0.0000001  #used for approximation
+    mid=(l+h)/2
+    
+    #run binary search
+    
+    while abs(mid**n-x)>=eps:    #running the loop till we minimise the difference between the number and the root
+        if x<mid**n:
+            h=mid
+        elif x>mid:
+            l=mid
+            
+        mid=(l+h)/2
+        
+    return mid
