8cb44fe8ed8b9929b99a5f2c1e8ee1d5ad7e0dcf
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-08-25 01:14:42 +0530

    inorder iterative

--- Day17/Inorder Traversal (with recursion and without recursion).py
+++ Day17/Inorder Traversal (with recursion and without recursion).py
@@ -6,3 +6,26 @@ def inOrder(self, root):
         print(root.data, end = " ")
         
         self.inOrder(root.right)
+        
+        
+        
+        
+#iterative solution        
+def inorderTraversal(self, root: TreeNode) -> List[int]:        
+        return_list = []
+        stack = []
+        node = root
+        
+        while True:
+            # Reach the left most Node of the current Node
+            if node is not None:
+                stack.append(node)
+                node = node.left
+                
+            elif(stack):
+                node = stack.pop()
+                return_list.append(node.val)
+                node = node.right
+            else:
+                break
+        return return_list
