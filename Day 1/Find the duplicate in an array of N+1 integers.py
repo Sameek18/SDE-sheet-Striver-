ddf08309c78478fe4c26aef27dae21f3e31cce75
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-08-25 18:16:06 +0530

    element that appears once in sorted array

--- Day 1/Find the duplicate in an array of N+1 integers.py
+++ Day 1/Find the duplicate in an array of N+1 integers.py
@@ -1,3 +1,26 @@
+#sorting
+#sort
+#check if two consqcutive numbers are equal
+def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+            
+#hashmap
+def findDuplicate(self, nums: List[int]) -> int:
+        d={}
+        #create dictionary from list
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+         #check if any value in dictinary is greater than 1
+        for k,v in d.items():
+            if v>1:
+                return k
+
 #Cycle method
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
---
+++ Day11/Find the element that appears once in sorted array, and rest element appears twice (Binary search).py
@@ -0,0 +1,49 @@
+#using hashing
+
+#O(n)
+
+def singleNonDuplicate(self, nums: List[int]) -> int:
+    d={}
+    #create dictionary
+    for i in nums:
+        if i not in d:
+            d[i]=1
+        else:
+            d[i]+=1
+    #print key,whose value is less than 2       
+    for k,v in d.items():
+        if v<2:
+            return k
+        
+            
+#Efficient approach           
+#Using Binary Search
+#O(log(n))
+
+
+#before single element:
+# 1st occ:even  ,2nd occ:odd
+
+#after single element:
+# 1st occ:odd  ,2nd occ:even
+
+def singleNonDuplicate(self, nums: List[int]) -> int:
+        l=0
+        h=len(nums)-1
+        while l<h:
+            mid=(l+h)//2
+            if mid%2==0:
+                if nums[mid]==nums[mid-1]:
+                    h=mid
+                else:
+                    l=mid
+            else:
+                if nums[mid]==nums[mid-1]:
+                    l=mid+1
+                else:
+                    h=mid-1
+                    
+        return nums[l]
+        
+
+
